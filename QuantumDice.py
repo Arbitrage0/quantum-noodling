7174c1b5c215e69ca7471c804b343a95c51fa871
Arbitrage0 <24595000+Arbitrage0@users.noreply.github.com>
2020-08-19 13:07:51 +0100

    Update file (ex2: apply to any-sided polyhedra)

--- QuantumDice.py
+++ QuantumDice.py
@@ -18,3 +18,22 @@ def throw_octahedral_die():
     temp = 2*temp + q
   
   return temp + 1 #shift 0 -> 1
+
+def throw_polyhedral_die(n): #extend to any-sided die
+  
+  qubits = int(math.ceil(math.log(n, 2)) #get number of qubits needed
+  qvm = get_qc(f"{qubits}q-qvm")
+  prog = Program() 
+  ro = prog.declare("ro", "BIT", qubits)
+  
+  for q in range(qubits):
+    prog += H(q)
+  for q in range(qubits):
+    prog.measure(q, ro[q])
+  
+  result = qvm.run(qvm.compile(prog))[0]
+  temp = 0
+  for q in result:
+    temp = 2*temp + q
+  
+  return int(math.floor(temp*(n/2**qubits))+1) #a bit of an unfair scaling down
